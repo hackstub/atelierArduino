ac08e26b8d26295d5a40b1defa543ec2d58dd8ef
Alexandre Aubin <alexandre.aubin@cern.ch>
2015-02-07 03:04:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Attention : le contenu est succeptible d'être modifié dans les prochains jours. Revenez plus tard pour une version plus finalisée.
+
+List des composants
+===================
+
+A faire
+
+Installer / utiliser Arduino
+============================
+
+A faire
---
+++ codeRobot.ino
@@ -0,0 +1,86 @@
+#include <Servo.h>  
+
+// #########################
+// #  Gestion des moteurs  #
+// #########################
+
+#define pinServoRoueGauche 9
+#define pinServoRoueDroite 10
+
+#define centerGauche 90
+#define centerDroite 90
+
+Servo servoRoueGauche;  // create servo object to control a servo 
+Servo servoRoueDroite;  // create servo object to control a servo 
+
+void initServos()
+{
+  servoRoueGauche.attach(pinServoRoueGauche);
+  servoRoueDroite.attach(pinServoRoueDroite);
+}
+ 
+void stopServos()
+{
+  servoRoueGauche.write(centerGauche);
+  servoRoueDroite.write(centerDroite);
+  servoRoueGauche.detach();
+  servoRoueDroite.detach();
+}
+
+// ######################
+// #  Fonction avancer  #
+// ######################
+
+void avancer(int vitesse, int duree)
+{
+  initServos();
+  servoRoueGauche.write(centerGauche+vitesse);
+  servoRoueDroite.write(centerDroite-vitesse);
+  delay(duree);
+  stopServos();
+}
+
+// ######################
+// #  Fonction tourner  #
+// ######################
+
+#define rotationEtalon 7150 // time to do 360° 
+
+void tourner(float fraction)
+{
+  int vitesse = 2.5;
+  int duree = (int) (rotationEtalon * fraction);
+  
+  initServos();
+  servoRoueGauche.write(centerGauche+vitesse);
+  servoRoueDroite.write(centerDroite+vitesse);
+  delay(duree);
+  stopServos();
+}
+
+// #########################
+// #  Pour la calibration  #
+// #########################
+
+void calibrationRepos()    { avancer(0,10000); }
+void calibrationRotation() { tourner(4); delay(10000); }
+  
+// ##################
+// #  Le programme  #
+// ##################
+
+void setup() 
+{ 
+}
+ 
+void loop() 
+{ 
+  
+  avancer(5,2000);
+  delay(1000);
+  tourner(0.25);
+  delay(1000);
+  
+  //calibrationRepos();
+  //calibrationRotation();
+}
